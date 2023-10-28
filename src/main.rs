e0cac4f6e95345747d1541e91d4ebcd7374c807e
prateek.jha <prateek.jha@udaan.com>
2023-10-29 03:03:19 +0530

    you need close stream to pass this

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
-use std::io::Write;
-use std::net::{Shutdown, TcpListener};
+// Uncomment this block to pass the first stage
+use std::net::{Shutdown, TcpListener, TcpStream};
+use std::io::{prelude::*};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    eprintln!("Logs from your program will appear here!");
+    println!("Logs from your program will appear here!");
+
+    // Uncomment this block to pass the first stage
+    //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
+            Ok(_stream) => {
                 println!("accepted new connection");
-                _stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).expect("TODO: panic message");
-                _stream.shutdown(Shutdown::Both).expect("TODO: panic message");
+                handle_connection(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let ok_response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write_all(ok_response.as_bytes()).unwrap();
+    stream.shutdown(Shutdown::Both).expect("TODO: panic message");
 }
